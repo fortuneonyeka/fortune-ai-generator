b10f1b00f2088d0f80926664cfddf8175668f96f
fortunatus <ihedfortune@gmail.com>
2024-09-04 09:34:48 +0100

    add custom sign-up and sign-in components and routes

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)',"/"])
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware((auth, req) => {
       if (isProtectedRoute(req)) auth().protect()
+      if (!isPublicRoute(req)) {
+            auth().protect()
+          }
     })
 
 export const config = {
