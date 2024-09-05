79cfe401c070fc5c6fe2d699d36ada60b6c39a2e
fortunatus <ihedfortune@gmail.com>
2024-09-05 12:12:17 +0100

    render SearchSection and TemplateListSection components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import SearchSection from './_components/SearchSection'
+import TemplateListSection from './_components/TemplateListSection'
 
 const Dashboard = () => {
+  const [userSearchInput, setUserSearchInput] = useState<string>()
+
+  const handleSearchInput = (value:string) => {
+    
+    setUserSearchInput(value);
+    
+    
+  }
+
   return (
-    <div>Dashboard</div>
+    <div>
+      {/* Search section */}
+      <SearchSection onSearchInput={handleSearchInput}/>
+
+      {/* Template list section */}
+      <TemplateListSection userSearchInput={userSearchInput}/>
+    </div>
   )
 }
 
