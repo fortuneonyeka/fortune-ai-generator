65097dc7f618e16263c894aaddb6a34a76c7b991
fortunatus <ihedfortune@gmail.com>
2024-09-10 13:51:19 +0100

    customise the component

--- app/dashboard/_components/TemplateCard.tsx
+++ app/dashboard/_components/TemplateCard.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Template } from './TemplateListSection'
+import { TEMPLATE } from './TemplateListSection'
 import Image from 'next/image'
 import  Link  from 'next/link'
 
-const TemplateCard = (item:Template) => {
+const TemplateCard = (item:TEMPLATE) => {
   return (
     <Link href={"/dashboard/content/"+item?.slug}>
     <div className='p-5 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] rounded-md border bg-white flex flex-col gap-3 cursor-pointer hover:scale-105 transition-all hover:rounded-lg'>
-      <Image src={item.icon} width={50} height={50} alt='icon' className=' self-center hover:scale-105 transition-all'/>
+      <Image src={item.icon} width={50} height={50} alt='icon' className=' self-center hover:scale-105 transition-all border-2 border-gray-500 rounded-full p-1 bg-gray-200'/>
       <h2 className='font-medium text-lg'>{item.name}</h2>
       <p className='text-gray-500 line-clamp-3'>{item.desc}</p>
     </div>
